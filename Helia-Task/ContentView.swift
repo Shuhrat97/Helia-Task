21464f8e9fa9dc735dc050ba421b2ab3aa624738
Shukhrat Nurov <shuhratnurov32@gmail.com>
2023-05-13 18:43:25 +0500

    Initial Commit

---
+++ Helia-Task/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Helia-Task
+//
+//  Created by Shuhrat Nurov on 13/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Helia-Task/Helia_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Helia_TaskApp.swift
+//  Helia-Task
+//
+//  Created by Shuhrat Nurov on 13/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Helia_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
